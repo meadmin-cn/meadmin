eae2e93c2e24d7c2ecbd9ac1847184fa73df48e3
zhangjiayun <zhangjiayun@homedo.com>
2023-06-15 23:22:18 +0800

    feat: cli 命令加上数据库实例加载

---
+++ packages/cli/src/utils/loadEnv.spec.ts
@@ -0,0 +1,34 @@
+import { loadEnvFile } from './loadEnv';
+import { join } from 'node:path';
+
+describe('加载单个env文件', () => {
+  it(`解析env`, async () => {
+    const env = loadEnvFile([join(__dirname, '../../test/.env')]);
+    expect(env.PORT).toEqual('4000');
+    expect(process.env.PORT).toEqual('4000');
+  });
+});
+
+describe('加载多个env文件', () => {
+  it('解析env', async () => {
+    const env = loadEnvFile([
+      join(__dirname, '../../test/.env.local1'),
+      join(__dirname, '../../test/.env.local2'),
+    ]);
+    expect(env.L_PORT).toEqual('2000');
+    expect(process.env.L_PORT).toEqual('2000');
+    expect(env.L_TIMEOUT).toEqual('5000');
+  });
+});
+
+describe('加载env变量文件', () => {
+  it('解析env', async () => {
+    const env = loadEnvFile([
+      join(__dirname, '../../test/.env.local1'),
+      join(__dirname, '../../test/.env.local2'),
+      join(__dirname, '../../test/.env.local3'),
+    ]);
+    expect(env.EMAIL).toEqual('support@5000');
+    expect(process.env.EMAIL).toEqual('support@5000');
+  });
+});
---
+++ packages/cli/src/utils/loadEnv.ts
@@ -0,0 +1,17 @@
+import * as fs from 'node:fs';
+import * as dotenv from 'dotenv';
+import { expand } from 'dotenv-expand';
+export function loadEnvFile(envFilePaths: string[]) {
+  const config = {} as Record<string, any>;
+  for (const envFilePath of envFilePaths) {
+    if (fs.existsSync(envFilePath)) {
+      const c = dotenv.parse(fs.readFileSync(envFilePath));
+      Object.assign(
+        config,
+        expand({ parsed: c  }).parsed,
+      );
+      console.log(envFilePath,c,config);
+    }
+  }
+  return config;
+}

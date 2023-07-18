3b054766d28882d67382be6914e6f458a50a4630
yuntian001 <yuntian001z@qq.com>
2023-07-18 17:21:20 +0800

    ignore: admin service 初步完善

--- src/app/admin/api/admin/admin.service.spec.ts
+++ src/app/admin/api/admin/admin.service.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from './admin.service';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { Admin } from './entities/admin.entity';
+import { CoreModule } from '@/app/core/core.module';
 
 describe('AdminService', () => {
   let service: AdminService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CoreModule.forRoot()],
       providers: [AdminService],
     }).compile();
 
@@ -14,5 +18,18 @@ describe('AdminService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('admin create', () => {
+    const createInfo = {
+      username: '123456',
+      nickname: 'test',
+      password: '123456',
+      email: '479820787@qq.com',
+      mobile: '15563556250',
+      status: '1',
+    };
+    console.log('createInfo', JSON.stringify(createInfo));
+    expect(service.create(createInfo)).toBeDefined();
   });
 });
--- src/app/admin/api/admin/admin.service.ts
+++ src/app/admin/api/admin/admin.service.ts
@@ -4,16 +4,54 @@ import { UpdateAdminDto } from './dto/update-admin.dto';
 import { Admin } from './entities/admin.entity';
 import { QueryAdminDto } from './dto/query-admin.dto';
 import { formatWhere } from '@/utils/formatWhere';
+import { pbkdf2Sync, randomBytes } from 'node:crypto';
 
 @Injectable()
 export class AdminService {
+  /**
+   * 密码加密
+   * @param password 密码
+   * @returns {salt:密码盐,password:密码密文}
+   */
+  private entityPassword(password: string) {
+    const salt = randomBytes(32);
+    const str = pbkdf2Sync(password, salt, 1000, 32, 'sha3-256').toString(
+      'hex',
+    );
+    return { salt: salt.toString('hex'), password: str };
+  }
+
+  /**
+   * 校验mime
+   * @param password 密码
+   * @param salt 密码盐
+   * @param encode 加密字符串
+   * @returns 是否通过
+   */
+  private checkPassword(password: string, salt: string, encode: string) {
+    return (
+      pbkdf2Sync(
+        password,
+        Buffer.from(salt, 'hex'),
+        1000,
+        16,
+        'sha3-256',
+      ).toString('hex') === encode
+    );
+  }
+
   /**
    * 创建数据
    * @param createAdminDto
    * @returns
    */
   create(createAdminDto: CreateAdminDto) {
-    return Admin.save(createAdminDto);
+    return Admin.save<Admin>(
+      Object.assign(
+        createAdminDto,
+        this.entityPassword(createAdminDto.password),
+      ),
+    );
   }
 
   /**

0ffc4f6737e408c1ef4fa6d7ef74f4ab43d71abe
yuntian001 <yuntian001z@qq.com>
2024-04-25 15:45:23 +0800

    feat: 初始化

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1714030878233_897',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;

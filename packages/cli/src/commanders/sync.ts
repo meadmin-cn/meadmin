5511aa55c27bf84ce67dadcbeeb1507934a4ec17
yuntian001 <yuntian001z@qq.com>
2025-05-29 19:04:21 +0800

    feat: 加上@meadmin/cli，并建立sync数据表同步命令

---
+++ packages/cli/src/commanders/sync.ts
@@ -0,0 +1,55 @@
+import { Command } from 'commander';
+import { join, resolve } from 'node:path';
+import { pathToFileURL } from 'node:url';
+import { importModels, Sequelize } from '@sequelize/core';
+import { Log } from '../utils/log.js';
+
+async function getConfig(dbConfig, name) {
+  const infos = await import(pathToFileURL(dbConfig).href);
+  const config = await (infos.__esModule ? infos.default : infos).default();
+  const configName = name ?? config.defaultDataSourceName ?? 'default';
+  return config.dataSource[configName];
+}
+
+export const syncInit =  (program: Command) => {
+  program
+    .command('sync')
+    .description('同步数据库结构')
+    .argument(
+      '<file>',
+      '需要同步的entity文件地址支持，隔开的多文件或glob,文件地址相对于项目dist/entities'
+    )
+    .option(
+      '-d, --dbConfig <char>',
+      '数据库配置文件地址默认为当前目录下dist/config/database.js',
+      join(process.cwd(), 'dist/config/database.js')
+    )
+    .option('-n, --name <char>', '使用的数据库配置defaultDataSourceName')
+    .action(async (file: string, options) => {
+      const files = file.split(',');
+      const config = Object.assign(
+        await getConfig(options.dbConfig, options.name),
+        {
+          logging: message=>Log.log(message),
+          models: await importModels(files.map(item =>
+            resolve(
+              process.cwd(),
+              'dist/entities/',
+              item.endsWith('.js') ||
+                item.endsWith('.mjs') ||
+                item.endsWith('.cjs') ||
+                item.endsWith('.ts') ||
+                item.endsWith('.mts') ||
+                item.endsWith('.cts')
+                ? item
+                : item + '.entity.js'
+            ).replace(/\\/g,'/')
+          )),
+        }
+      );
+      const sequelize = new Sequelize(config);
+      await sequelize.sync({alter:true});
+      sequelize.close();
+      Log.success('同步数据表结构成功');
+    });
+};
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,26 @@
+import { Command } from 'commander';
+import { readFileSync } from 'node:fs';
+import {syncInit} from './commanders/index.js';
+import dotenv from 'dotenv';
+// 根据当前环境加载不同的 .env 文件
+if (process.env.NODE_ENV) {
+  dotenv.config({
+    path: ['.env', `.env.${process.env.NODE_ENV}`],
+    override: true,
+  });
+} else {
+  dotenv.config({
+    path: ['.env'],
+    override: true,
+  });
+}
+const program = new Command();
+const { version } = JSON.parse(
+  readFileSync(import.meta.dirname + '/../package.json').toString(),
+);
+program
+  .name('meadmin')
+  .description('CLI of MEADMIN')
+  .version(version);
+syncInit(program);
+program.parse();

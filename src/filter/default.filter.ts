7d80895e4a369e5811eeba817b7578df5af709b9
yuntian001 <yuntian001z@qq.com>
2025-06-10 10:05:53 +0800

    feat: 异常时日志记录

--- src/filter/default.filter.ts
+++ src/filter/default.filter.ts
@@ -1,19 +1,21 @@
 import { CodeEunm } from '@/dict/code.enum.js';
 import { ResponseService } from '@/service/response.service.js';
-import { Catch, Config } from '@midwayjs/core';
+import { Catch, Config, ILogger, Logger } from '@midwayjs/core';
 
 @Catch()
 export class DefaultErrorFilter {
   @Config('debug')
   debug?: boolean;
-
+  @Logger()
+  logger: ILogger;
   protected resposes: ResponseService;
 
   constructor() {
     this.resposes = new ResponseService();
   }
-
+ 
   async catch(err: Error) {
+    this.logger.error(err);
     // 所有的未分类错误会到这里
     return this.resposes.error(
       this.debug ? err.message : '未知异常请联系管理员',

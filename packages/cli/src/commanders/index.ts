b1c6c42e0e9b94d20ad1d5cf7bc5b56548114c31
yuntian001 <yuntian001z@qq.com>
2025-08-18 19:36:11 +0800

    feat: 加上-m --module 参数设置env环境变量

--- packages/cli/src/commanders/index.ts
+++ packages/cli/src/commanders/index.ts
@@ -1,7 +1,12 @@
 import { Command } from 'commander';
 import { crudInit } from './crud.js';
 import { syncInit } from './sync.js';
+import { setEnv } from '../utils/env.js';
 export const init = (program:Command)=>{
+    program.option('-m, --module <char>', 'env环境变量')
+    .hook('preAction', (thisCommand) => {
+        setEnv(thisCommand.opts().module);
+    });
     syncInit(program);
     crudInit(program);
 }

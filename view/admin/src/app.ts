0435f8e853060b645630b7e9479022c9bdb49741
yuntian001 <yuntian001z@qq.com>
2025-10-13 20:22:34 +0800

    fix: menuItem改为全局组件

--- view/admin/src/app.ts
+++ view/admin/src/app.ts
@@ -3,10 +3,13 @@ import '@/event/module';
 import { event, mitter } from './event';
 import { App } from 'vue';
 export let app: App;
+import layoutMenuItem from '@/layout/components/menu/components/menuItem.vue';
 export async function bootscrapt(appObj: App) {
   app = appObj;
   appObj.config.globalProperties.$start = true;
   await Promise.allSettled(mitter.emit(event.START, appObj));
+  app.component('LayoutMenuItem', layoutMenuItem);
+
   //忽略resolveComponent can only be used in render() or setup().的警告
   appObj.config.warnHandler = (msg, instance, trace) => {
     if (msg !== 'resolveComponent can only be used in render() or setup().') {

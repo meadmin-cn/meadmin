98b705f9393c084e6ad80e7295f8e313348accb5
yuntian001 <yuntian001z@qq.com>
2026-01-03 01:16:57 +0800

    fix: 菜单和角色更新dto排除自动创建字段

--- src/app/admin/dto/system/menuUpdate.dto.ts
+++ src/app/admin/dto/system/menuUpdate.dto.ts
@@ -1,6 +1,8 @@
 import { InferAttributesLoose } from '@/../types/entity.js';
 import { SystemMenu } from '../../../../entities/systemMenu.entity.js';
-import { PartialType } from '@/helper/dto.js';
+import { OmitDtoType, PartialType } from '@/helper/dto.js';
 
 //dto参数校验继承 entity必须使用 PickDtoType|OmitDtoType|PartialType|RequiredType|IntersectionType 之一 否则不会生效
-export class SystemMenuUpdateDto extends PartialType(SystemMenu as new () => InferAttributesLoose<SystemMenu>) {}
+export class SystemMenuUpdateDto extends PartialType(OmitDtoType(SystemMenu as new () => InferAttributesLoose<SystemMenu>,
+  ['id','createdAt','updatedAt','createdAdminId','updatedAdminId'])){ //排除自动创建的字段
+}
--- src/app/admin/dto/system/roleUpdate.dto.ts
+++ src/app/admin/dto/system/roleUpdate.dto.ts
@@ -5,7 +5,8 @@ import { ApiPropertyRule } from '@/decorators/index.js';
 import { RuleType } from '@/ruleType/index.js';
 
 //dto参数校验继承 entity必须使用 PickDtoType|OmitDtoType|PartialType|RequiredType|IntersectionType 之一 否则不会生效
-export class SystemRoleUpdateDto extends PartialType(OmitDtoType(SystemRole as new () => InferAttributesLoose<SystemRole>, ['menus'])) {
+export class SystemRoleUpdateDto extends PartialType(OmitDtoType(SystemRole as new () => InferAttributesLoose<SystemRole>,
+ ['menus','id','createdAt','updatedAt','createdAdminId','updatedAdminId'])){ //排除自动创建的字段
   @ApiPropertyRule({ description: '具有权限的菜单id', rule: RuleType.array().items(RuleType.string()) })
   menuIds:string[]
 }

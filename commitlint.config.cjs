79e2b1293b56b5382e530d8b2c68b957714f9bdd
yuntian001 <yuntian001z@qq.com>
2026-01-28 20:15:52 +0800

    refactor: 优化commitlint规则读取

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,23 @@
 const releaseItConfig = require('./.release-it.json');
+
+const enmus = {};
+releaseItConfig.plugins['@release-it/conventional-changelog'].preset.types.forEach((item) => {
+  enmus[item.type] = {
+    description: item.section,
+    title: item.type,
+  };
+});
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-empty': [2, 'never'], // type 不为空
     'type-enum': [2, 'always', releaseItConfig.plugins['@release-it/conventional-changelog'].preset.types.map((item) => item.type)],
+  },
+  prompt: {
+    questions: {
+      type: {
+        enum: enmus,
+      },
+    },
   },
 };

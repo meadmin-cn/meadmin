88feec8e4b13f9f07d3339e770c639a37c4a6032
yuntian001 <yuntian001z@qq.com>
2026-03-25 21:01:51 +0800

    feat(create-meadmin): 加上批量设置版本功能

--- packages/create-meadmin/src/commanders/index.ts
+++ packages/create-meadmin/src/commanders/index.ts
@@ -1,5 +1,7 @@
 import { Command } from 'commander';
 import { setMeadminTemplate } from './setMeadminTemplate.js';
+import { version } from './version.js';
 export const init = (program: Command) => {
   setMeadminTemplate(program);
+  version(program);
 };

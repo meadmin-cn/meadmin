94f302f3a1ccbca275913a3e67d563193fc693f8
yuntian001 <yuntian001z@qq.com>
2026-04-21 20:16:01 +0800

    feat: 加上控制台示例页

---
+++ view/admin/src/views/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dashboard">
+    <total-panel></total-panel>
+    <charts></charts>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Charts from './components/charts.vue';
+import TotalPanel from './components/totalPanel.vue';
+</script>
+<style lang="scss" scoped>
+.dashboard {
+}
+</style>

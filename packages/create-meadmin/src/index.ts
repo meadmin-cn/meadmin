67d4c001f3ca7e6ce63064983ca4dcda9e93f018
yuntian001 <yuntian001z@qq.com>
2026-05-19 18:18:51 +0800

    feat(create-meadmin): 加上 创建数据库输入识别

--- packages/create-meadmin/src/index.ts
+++ packages/create-meadmin/src/index.ts
@@ -35,42 +35,47 @@ try {
     }),
   );
   console.log('项目文件创建成功...');
-  const r2 = readlinePromises.createInterface({ input, output });
-  const createDb = (await r2.question('请在.env文件中完善数据库连接信息，完善完成后输入y同步数据库信息。(输入n暂不同步数据库，稍后自行导入meadmin.sql进行数据库同步)\n')) ?? '';
-  r2.close();
-  if (['y', 'Y', 'yes', 'YES'].includes(createDb)) {
-    dotenv.config({
-      path: ['.env'],
-      override: true,
-    });
-    const DATABASE_HOST = process.env.DATABASE_HOST;
-    const DATABASE_PORT = process.env.DATABASE_PORT;
-    const DATABASE_DB = process.env.DATABASE_DB;
-    const DATABASE_SCHEMA = process.env.DATABASE_SCHEMA;
-    const DATABASE_USER = process.env.DATABASE_USER;
-    const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
-    console.log('正在同步数据库结构...');
-    const sequelize = new Sequelize({
-      dialect: PostgresDialect,
-      database: DATABASE_DB,
-      user: DATABASE_USER,
-      password: DATABASE_PASSWORD,
-      host: DATABASE_HOST,
-      port: +DATABASE_PORT,
-      options: `-c search_path=${DATABASE_SCHEMA}`,
-      logging: debug
-        ? (sql, timing) => {
-            console.log(new Date(), `[${timing}]:${sql}`);
-          }
-        : undefined,
-      benchmark: true, //开启日志打印sql耗时参数传递
-    });
-    await sequelize.queryRaw(sql, { raw: true, type: QueryTypes.RAW });
-    await sequelize.close();
-    console.log('数据库同步完成');
-  }
+  const createDbFn = async () => {
+    const r2 = readlinePromises.createInterface({ input, output });
+    const createDb = (await r2.question('请在.env文件中完善数据库连接信息，完善完成后输入y同步数据库信息。(输入n暂不同步数据库，稍后自行导入meadmin.sql进行数据库同步)\n')) ?? '';
+    r2.close();
+    if (['y', 'Y', 'yes', 'YES'].includes(createDb)) {
+      dotenv.config({
+        path: ['.env'],
+        override: true,
+      });
+      const DATABASE_HOST = process.env.DATABASE_HOST;
+      const DATABASE_PORT = process.env.DATABASE_PORT || 0;
+      const DATABASE_DB = process.env.DATABASE_DB;
+      const DATABASE_SCHEMA = process.env.DATABASE_SCHEMA;
+      const DATABASE_USER = process.env.DATABASE_USER;
+      const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+      console.log('正在同步数据库结构...');
+      const sequelize = new Sequelize({
+        dialect: PostgresDialect,
+        database: DATABASE_DB,
+        user: DATABASE_USER,
+        password: DATABASE_PASSWORD,
+        host: DATABASE_HOST,
+        port: +DATABASE_PORT,
+        options: `-c search_path=${DATABASE_SCHEMA}`,
+        logging: debug
+          ? (sql, timing) => {
+              console.log(new Date(), `[${timing}]:${sql}`);
+            }
+          : undefined,
+        benchmark: true, //开启日志打印sql耗时参数传递
+      });
+      await sequelize.queryRaw(sql, { raw: true, type: QueryTypes.RAW });
+      await sequelize.close();
+      console.log('数据库同步完成');
+    } else if (!['n', 'N', 'no', 'NO'].includes(createDb)) {
+      await createDbFn();
+    }
+  };
+  await createDbFn();
   // 同步生成子进程，运行npm命令
-  const { error } = spawn.sync('pnpm', '--version', { stdio: 'inherit' });
+  const { error } = spawn.sync('pnpm', ['--version'], { stdio: 'inherit' });
   console.log(`创建成功，项目必须使用pnpm启动，请执行以下命令调试项目。
 ${error ? '- npm install -g pnpm\n' : '\n'}- pnpm install
 - pnpx husky install (可选，执行后会初始化git提交格代码式化钩子) 
